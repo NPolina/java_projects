16b7703673376b0c17a75d324e79a7d059f1de24
NPolina <nazarova.p@avantajprim.com>
2016-04-14 20:51:50 +0300

    Подключение тестового фреймворка и создание тестов для класса Point

---
+++ sandbox/src/test/java/qa/project/sandbox/PointTests.java
@@ -0,0 +1,33 @@
+package qa.project.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by user on 14.04.2016.
+ */
+public class PointTests {
+
+    Point p1 = new Point(-1, -3);
+    Point p2 = new Point(2, -7);
+    Point p3 = new Point(2,3);
+    Point p4 = new Point(10,9);
+
+    @Test
+    public void testDistanceX() {
+        Assert.assertEquals(p1.distanceX(p2.x),9.0);
+        Assert.assertEquals(p3.distanceX(p4.x),64.0);
+    }
+
+    @Test
+    public void testDistanceY(){
+        Assert.assertEquals(p1.distanceY(p2.y),16.0);
+        Assert.assertEquals(p3.distanceY(p4.y),36.0);
+    }
+
+    @Test
+    public void testDistance(){
+        Assert.assertEquals(p1.distance(p2),5.0);
+        Assert.assertEquals(p3.distance(p4),10.0);
+    }
+}
