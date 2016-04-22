8911d39638e5fb7324ae941585c66d8a8111f4bf
NPolina <nazarova.p@avantajprim.com>
2016-04-22 20:39:50 +0300

    Дефолтные значения полей при создании новой группы

--- addressbook-web-tests/src/test/java/qa/project/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/qa/project/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,6 @@ package qa.project.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import qa.project.addressbook.model.GroupData;
 
 /**
