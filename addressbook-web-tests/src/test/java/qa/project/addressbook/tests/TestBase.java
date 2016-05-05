ce552cc87ca50ca7b69cceba3cf160fd21d3e31f
NPolina <nazarova.p@avantajprim.com>
2016-05-05 19:36:30 +0300

    Запуск нескольких тестов в 1 браузере

--- addressbook-web-tests/src/test/java/qa/project/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/project/addressbook/tests/TestBase.java
@@ -2,22 +2,26 @@ package qa.project.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import qa.project.addressbook.appmanager.ApplicationManager;
+
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 
 /**
  * Created by user on 15.04.2016.
  */
 public class TestBase {
 
-   protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+   protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
