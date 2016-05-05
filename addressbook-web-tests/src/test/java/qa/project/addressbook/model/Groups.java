c1f28c28a85ae34447549a7dd1682eb859245c87
NPolina <nazarova.p@avantajprim.com>
2016-05-05 22:50:30 +0300

    Проверка через Hamcrest

---
+++ addressbook-web-tests/src/test/java/qa/project/addressbook/model/Groups.java
@@ -0,0 +1,38 @@
+package qa.project.addressbook.model;
+
+import com.google.common.collect.ForwardingSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by user on 05.05.2016.
+ */
+public class Groups extends ForwardingSet<GroupData> {
+    private Set<GroupData> delegate;
+
+    public Groups(Groups groups) {
+        this.delegate = new HashSet<GroupData>(groups.delegate);
+    }
+
+    public Groups() {
+        this.delegate = new HashSet<>();
+    }
+
+    @Override
+    protected Set<GroupData> delegate() {
+        return delegate;
+    }
+
+    public Groups withAdded(GroupData group){
+        Groups groups = new Groups(this);
+        groups.add(group);
+        return groups;
+    }
+
+    public Groups without(GroupData group){
+        Groups groups = new Groups(this);
+        groups.add(group);
+        return groups;
+    }
+}
